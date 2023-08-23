8cd625e79bfaa3f5e51ec307da50dff90c17b8b7
Veritas93 <loli93@yandex.ru>
2023-08-23 19:52:29 +0300

    Done exercise

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |n| n.positive? ? array.max : n }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        first = 0
+        last = array.length - 1
+
+        while first <= last
+          i = (first + last) / 2
+
+          if array[i] == query
+            return i
+          elsif array[i] > query
+            last = i - 1
+          else
+            array[i] < query
+            first = i + 1
+          end
+        end
+        -1
       end
     end
   end
--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,35 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        for i in 0..(length - 1)
+          yield(self[i])
+        end
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        result = MyArray.new
+        my_each { |n| result << yield(n) }
+        result
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        result = MyArray.new
+        my_each { |n| result << n unless n.nil? }
+        result
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(init = nil)
+        acc = self[0]
+        for i in 1..(length - 1)
+          acc = yield(acc, self[i])
+        end
+        init ? yield(init, acc) : acc
+      end
     end
   end
 end
