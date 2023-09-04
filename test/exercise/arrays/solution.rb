f720ea55266004e99475190b73a7a33b3771bc06
Veritas93 <loli93@yandex.ru>
2023-09-04 16:35:43 +0300

    Reweiw cod

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -3,42 +3,24 @@ module Exercise
     class << self
       def get_max_element(array)
         max_element = array[0]
-        for i in array do
-          max_element = i if i > max_element
-        end
+        array.each { |element| max_element = element if max_element < element }
         max_element
       end
 
       def replace(array)
         max_element_in_array = get_max_element(array)
         result = []
-        for i in array do
-          new_i = (i.positive? ? max_element_in_array : i)
-          result << new_i
-        end
+        array.each { |element| result << (element.positive? ? max_element_in_array : element) }
         result
       end
 
-      def get_binary_search(array, query, low, higth)
-        if array.empty? || low > higth
-          -1
-        else
-          middpoint = ((low + higth) / 2).to_i
+      def search(array, query, low = 0, higth = array.length - 1)
+        return -1 if array.empty? || array[0] > query || array[- 1] < query || low > higth
 
-          return middpoint if query == array[middpoint]
-
-          if query < array[middpoint]
-            new_higth = middpoint - 1
-            get_binary_search(array, query, low, new_higth)
-          else
-            new_low = middpoint + 1
-            get_binary_search(array, query, new_low, higth)
-          end
-        end
-      end
-
-      def search(array, query)
-        get_binary_search(array, query, 0, array.length - 1)
+        middpoint = ((low + higth) / 2).to_i
+        return middpoint if query == array[middpoint]
+        return search(array, query, low, middpoint - 1) if query < array[middpoint]
+        return search(array, query, middpoint + 1, higth) if query > array[middpoint]
       end
     end
   end
